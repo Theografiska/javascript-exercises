bf9ee8ed3d85e822113322730a1ce480aa1dffa0
Theodor Kaljo <theodor.kaljo@gmail.com>
2024-12-07 16:28:20 +0200

    Pass all the tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
-
+const repeatString = (string, number) => {
+    let phrase = "";
+    if (number<0) {
+        return 'ERROR';
+    } else {
+    for (i=1; i<=number; i++) {
+        phrase += string;
+    }
+    return phrase;
+    }
 };
+
+let repeatedPhrase = repeatString('John', 4);
+console.log(repeatedPhrase);
 
 // Do not edit below this line
 module.exports = repeatString;
