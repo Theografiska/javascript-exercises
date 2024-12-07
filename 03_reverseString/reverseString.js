002618ad420a6b63022ac3ea9cb22d2314640667
Theodor Kaljo <theodor.kaljo@gmail.com>
2024-12-07 16:38:46 +0200

    Added the functions to complete all 4 tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = string => {
+    const splitToArray = string.split("");
+    let newString = "";
+    for (i=splitToArray.length-1; i>=0; i--) {
+        newString += splitToArray[i];
+    } 
+    return newString;
 };
 
 // Do not edit below this line
