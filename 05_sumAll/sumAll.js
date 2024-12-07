8875d2efe78d5a0dd35729a2b90fa875214106d7
Theodor Kaljo <theodor.kaljo@gmail.com>
2024-12-07 17:32:03 +0200

    Solve all tests, using Number.isInteger(x) method and for loops

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = (firstNo, secondNo) => {
+    let sum = 0;
+    if (firstNo <= 0 || secondNo <= 0) {
+        return 'ERROR';
+    } else if (Number.isInteger(firstNo) === false || Number.isInteger(secondNo) === false) {
+        return 'ERROR';
+    } else if (firstNo === secondNo) {
+        sum += firstNo + secondNo;
+    } else if (firstNo < secondNo) {
+        for (i=firstNo; i<=secondNo; i++) {
+            sum += i;
+        }
+    } else if (secondNo < firstNo) {
+        for (i=secondNo; i<=firstNo; i++) {
+            sum += i;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
